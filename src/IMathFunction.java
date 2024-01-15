310e47463ba5a57305dd0257fe755c78536600f6
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-01-15 17:04:06 +0530

    Math operation using lambda

---
+++ src/IMathFunction.java
@@ -0,0 +1,8 @@
+public interface IMathFunction {
+    @FunctionalInterface
+    public interface IMathFuction {
+        //FunctionalInterface is interface having only single method declaration.
+        int calculate(int a,int b);  //Method declaration
+    }
+
+}
--- src/MathOperation.java
+++ src/MathOperation.java
@@ -1,6 +1,19 @@
-public class MathOperation {
+public class MathOperation implements IMathFunction{
     public static  void main(String[] args)
     {
-        System.out.println("Lambda expression");
+        /* UC1.1 Develop Math Operation App to perform Math Functions Addition,
+Subtraction and Division - Use Lambda Expression to perform Math Operation*/
+
+        IMathFuction addition=(a,b)-> (a+b);
+        IMathFuction multiply =(a,b)-> (a*b);
+        IMathFuction divide=(a,b)->(a/b);
+            //Divide is lamba expression that takes  a,b (as parametres) & returns a/b;
+
+            System.out.println("Addition is"+addition.calculate(100,200));
+            System.out.println("division  is:"+divide.calculate(200,5));
+            System.out.println("Mutliplication  is:"+multiply.calculate(10,20));
+        }
+
     }
-}
+
+
