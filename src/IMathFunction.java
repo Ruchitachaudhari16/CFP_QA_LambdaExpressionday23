d1b562c002f2c0159000f1edc71983be87133da8
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-01-16 13:29:13 +0530

    Show result using lambda function

--- src/IMathFunction.java
+++ src/IMathFunction.java
@@ -1,8 +1,17 @@
-public interface IMathFunction {
-    @FunctionalInterface
-    public interface IMathFuction {
+
+@FunctionalInterface
+    public interface IMathFunction {
         //FunctionalInterface is interface having only single method declaration.
         int calculate(int a,int b);  //Method declaration
+    static void printResult(int a,int b,String  function,IMathFunction fobj)
+    {
+        System.out.println("Result of"+function+" is "+fobj.calculate(a, b));
     }
+    }
+    //Passing lambda expression as function parameter to print result using static function.
 
-}
+
+
+
+
+
--- src/MathOperation.java
+++ src/MathOperation.java
@@ -1,19 +1,27 @@
-public class MathOperation implements IMathFunction{
-    public static  void main(String[] args)
-    {
+public class MathOperation {
+    public static void main(String[] args) {
         /* UC1.1 Develop Math Operation App to perform Math Functions Addition,
 Subtraction and Division - Use Lambda Expression to perform Math Operation*/
 
-        IMathFuction addition=(a,b)-> (a+b);
-        IMathFuction multiply =(a,b)-> (a*b);
-        IMathFuction divide=(a,b)->(a/b);
-            //Divide is lamba expression that takes  a,b (as parametres) & returns a/b;
+        IMathFunction addition = (a, b) -> (a + b);
+        IMathFunction subtraction =(a,b) -> (a - b);
+        IMathFunction multiply = (a, b) -> (a * b);
+        IMathFunction divide = (a, b) -> (a / b);
+        //Divide is lamba expression that takes  a,b (as parametres) & returns a/b;
+//UC1.1:- Add and multiply using lambda expression
+        System.out.println("Addition is" + addition.calculate(100, 200));
+        System.out.println("division  is:" + divide.calculate(200, 5));
+        System.out.println("Mutliplication  is:" + multiply.calculate(10,20));
+        System.out.println("Subtraction is:" +subtraction.calculate(1000,500));
 
-            System.out.println("Addition is"+addition.calculate(100,200));
-            System.out.println("division  is:"+divide.calculate(200,5));
-            System.out.println("Mutliplication  is:"+multiply.calculate(10,20));
-        }
+
+        //UC.1.2:-Passig lambda expression as a function to print result using static function
+        IMathFunction.printResult(100, 200, "Addition", addition);
+        //IMathFunction.printResult(220,100,"Subtraction",subtract);
+        IMathFunction.printResult(500, 200, "Division",divide);
+        IMathFunction.printResult(100, 200, "Multiply",multiply);
 
     }
+}
 
 
